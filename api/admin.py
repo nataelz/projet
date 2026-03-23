8d6916965e7c8e5c0e7d3ca5fdaac03d982dfa51
nataelz <natael.zerbib@gmail.com>
2026-03-23 16:59:06 +0100

    Update the model

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Company, Component, Processor, Computer
+from .models import Company, Component, Processor, Memory, Storage, GraphicsCard, Network, Computer
 
 # Register your models here.
 admin.site.register(Company)
 admin.site.register(Component)
 admin.site.register(Processor)
+admin.site.register(Memory)
+admin.site.register(Storage)
+admin.site.register(GraphicsCard)
+admin.site.register(Network)
 admin.site.register(Computer)
--- api/models.py
+++ api/models.py
@@ -1,48 +1,91 @@
 from django.db import models
 
 class Company(models.Model):
-    name = models.CharField("name of the company", max_length=200)
+    name = models.CharField(max_length=100)
+    website = models.URLField(blank=True)
 
+    class Meta:
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
+        ordering = ["name"]
+    
     def __str__(self):
         return self.name
 
 class Component(models.Model):
-    name = models.CharField("name of the component", max_length=200)
-    constructor = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, blank=True)
+    constructor = models.ForeignKey(Company, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.constructor} {self.name}"
 
 class Processor(Component):
-    core_count = models.PositiveSmallIntegerField("Number of cores")
+    ARCHITECTURE_CHOICES = [
+        ("x86", "x86 (32-bit)"),
+        ("x86-64", "x86-64 (64-bit)"),
+        ("arm", "ARM")
+    ]
+
+    architecture = models.CharField(max_length=10, choices=ARCHITECTURE_CHOICES)
+    frequency = models.PositiveBigIntegerField()
+    core_count = models.PositiveSmallIntegerField()
 
 class Memory(Component):
-    MEMORY_KIND_CHOICES = {
-        "1": "DDR1",
-        "2": "DDR2",
-        "3": "DDR3",
-        "4": "DDR4",
-        "5": "DDR5"
-    }
+    MEMORY_TYPE = (
+        ("sdr", "SDR"),
+        ("ddr1", "DDR1"),
+        ("ddr2", "DDR2"),
+        ("ddr3", "DDR3"),
+        ("ddr4", "DDR4"),
+        ("ddr5", "DDR5")
+    )
+    size = models.PositiveBigIntegerField()
+    frequency = models.PositiveBigIntegerField()
 
-    kind = models.CharField(max_length=1, choices=MEMORY_KIND_CHOICES)
-    size = models.PositiveBigIntegerField("Capacity")
-    speed = models.PositiveBigIntegerField("Speed")
+class Storage(Component):
+    STORAGE_TYPE_CHOICES = [
+        ("hdd", "HDD"),
+        ("ssd", "SSD"),
+        ("nvme", "NVMe"),
+        ("eMMC", "eMMC")
+    ]
 
-class Drive(Component):
-    DRIVE_KIND_CHOICES = {
-        "H": "HDD",
-        "S": "SSD"
-    }
-    kind = models.CharField(max_length=1, choices=DRIVE_KIND_CHOICES)
-    size = models.PositiveBigIntegerField("Capacity")
+    storage_type = models.CharField(max_length=10, choices=STORAGE_TYPE_CHOICES)
+    size = models.PositiveBigIntegerField()
 
 class GraphicsCard(Component):
     pass
 
-class Computer(models.Model):
-    name = models.CharField("name of the computer", max_length=200)
-    components = models.ManyToManyField(Component)
+class Network(Component):
+    NETWORK_TYPE_CHOICES = [
+        ("ethernet", "Ethernet"),
+        ("wifi", "Wi-Fi")
+    ]
 
-    def __str__(self):
-        return self.name
+    network_type = models.CharField(max_length=10, choices=NETWORK_TYPE_CHOICES)
+    speed = models.PositiveBigIntegerField()
+
+class Computer(models.Model):
+    FORMAT_CHOICES = [
+        ("desktop", "Desktop"),
+        ("laptop", "Laptop"),
+        ("rack", "Rack-Mounted"),
+        ("mini", "Mini PC"),
+        ("tablet", "Tablet")
+    ]
+
+    constructor = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=150, blank=True)
+    model_number = models.CharField(max_length=150, blank=True)
+    serial_number = models.CharField(max_length=150, blank=True)
+    format = models.CharField(max_length=15, choices=FORMAT_CHOICES)
+
+    processors = models.ManyToManyField(Processor)
+
+    memory = models.ManyToManyField(Memory)
+
+    storage = models.ManyToManyField(Storage)
+
+    graphics_card = models.ManyToManyField(GraphicsCard)
+
+    network = models.ManyToManyField(Network)
